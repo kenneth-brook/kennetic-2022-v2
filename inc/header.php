0dc6e1c0bf42ea9e6028e505513c64564670b5ee
Kenneth Brook <kenneth.brook@hotmail.com>
2022-04-09 20:22:11 -0400

    header animation made

--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,11 @@
 <header>
     <div class="headerWrap">
         <img src="./img/kclogo.png" />
-        <div class="headFlag"></div>
+        <div class="headFlag">
+            <div class="wipeText">
+                <h1>Kennetic Concepts</h1>
+                <h3>Bringing An Impact To Technology Startups</h3>
+            </div>
+        </div>
     </div>
 </header>
