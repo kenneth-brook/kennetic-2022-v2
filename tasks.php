6d2e7cca54266808c4737c15bf9282971814d67e
Kenneth Brook <kenneth.brook@hotmail.com>
2022-04-24 11:26:35 -0400

    half way though task imput form creation

--- tasks.php
+++ tasks.php
@@ -1,3 +1,29 @@
+<?php
+    include "inc/db_config.php";
+
+    if(isset($_POST['submit'])) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $importance = $_POST['importance'];
+        $difficulty = $_POST['difficulty'];
+    }
+
+    $sql = "INSERT INTO 'tasks' ('title', 'description', 'importance', 'difficulty') VALUES ('$title', '$description', '$importance', '$difficulty')";
+
+    $result = $connection->query($sql);
+
+    if($result == TRUE) {
+        echo "We Good";
+    }
+    else {
+        echo "It Broke:" . $sql . "<br>". $connection->error;
+    }
+
+    $connection->close();
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,8 +39,20 @@
     <?php include 'inc/header-script.php'; ?>
 </head>
 
-<body id="home">
+
+
+<body id="task">
     <?php include 'inc/header.php'; ?>
+
+    <h2>Task Entry</h2>
+
+    <form action="" method="POST">
+        <fieldset>
+            <legend>Task Information:</legend>
+            Task Name:<br>
+            <input type="text" name="title">
+            <br>
+
 </body>
 <?php include 'inc/footer.php'; ?>
 
