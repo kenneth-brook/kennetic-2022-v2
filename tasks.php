038037fc7e4f6cee38a3f7fbff947d30c781e446
Kenneth Brook <kenneth.brook@hotmail.com>
2022-04-25 20:11:23 -0400

    added WYSIWYG editor

--- tasks.php
+++ tasks.php
@@ -1,3 +1,6 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
 <?php
     include "inc/db_config.php";
 
@@ -23,10 +26,6 @@
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
@@ -36,13 +35,32 @@
 
     <title>Tasks | Kennetic Concepts</title>
 
+    <script src="./js/tinymce/tinymce.min.js"></script>
+
     <?php include 'inc/header-script.php'; ?>
+
+    <script>
+    tinymce.init({
+        selector: '#mytextarea',
+        plugins: [
+            'a11ychecker', 'advlist', 'advcode', 'advtable', 'autolink', 'checklist', 'export',
+            'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+            'powerpaste', 'fullscreen', 'formatpainter', 'insertdatetime', 'media', 'table', 'help',
+            'wordcount'
+        ],
+        toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
+            'alignleft aligncenter alignright alignjustify | ' +
+            'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+    });
+    </script>
 </head>
 
 
 
 <body id="task">
     <?php include 'inc/header.php'; ?>
+
+
 
     <h2>Task Entry</h2>
 
@@ -51,7 +69,12 @@
             <legend>Task Information:</legend>
             Task Name:<br>
             <input type="text" name="title">
-            <br>
+            <br><br>
+            <textarea id="mytextarea">Hello, World!</textarea>
+            <br><br>
+            <input type="submit" name="submit" value="submit">
+        </fieldset>
+    </form>
 
 </body>
 <?php include 'inc/footer.php'; ?>
