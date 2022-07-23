f82a10d04ed16a58846079dfbac0c63e692c5fd2
Kenneth Brook <kenneth.brook@hotmail.com>
2022-07-23 18:12:22 -0400

    posted by bug corrected

--- inc/home-news.php
+++ inc/home-news.php
@@ -1,16 +1,19 @@
 <?php
 include_once('db_config.php');
 
-$sub_sql='SELECT * FROM news WHERE status = "published" ORDER BY created_on DESC';
+$sub_sql="SELECT news.owner, news.created_on, news.title, news.content, directus_users.id, directus_users.first_name
+          FROM news 
+          INNER JOIN directus_users
+          ON news.owner = directus_users.id
+          ORDER BY news.created_on DESC";
 
     $sub_result = mysqli_query($connection,$sub_sql);
 
     while ($sub_rows = mysqli_fetch_assoc($sub_result)) {
-        $owner = $sub_rows['owner'];
+        $owner = $sub_rows['first_name'];
         $date = $sub_rows['created_on'];
         $title = $sub_rows['title'];
         $content = $sub_rows['content'];
-
 
         echo "<div class='newsCard'>
             <div class='newsTop'>
