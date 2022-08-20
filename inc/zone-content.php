51d186ecb800ed1d28807d4f0a4c1511e5e8c74a
Kenneth Brook <kenneth.brook@hotmail.com>
2022-08-20 16:32:45 -0400

    zone page created

---
+++ inc/zone-content.php
@@ -0,0 +1,29 @@
+<div class="zone t13">
+    <div class="zone t12">
+        <div class="zone t11">
+            <div class="zone t10">
+                <div class="zone t9">
+                    <div class="zone t8">
+                        <div class="zone t7">
+                            <div class="zone t6">
+                                <div class="zone t5">
+                                    <div class="zone t4">
+                                        <div class="zone t3">
+                                            <div class="zone t2">
+                                                <div class="zone t1">
+                                                    <div class="zone board">
+                                                        <p>BOARD</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ zones.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="Description" content="" />
+
+    <title>Home | Kennetic Concepts</title>
+
+    <?php include 'inc/header-script.php'; ?>
+</head>
+
+<body id="home">
+    <?php include 'inc/header.php'; ?>
+    <div class="homeContWrap">
+        <div class="eventMenu">
+            <div class="titleBlock">
+                <h4>Upcoming Events</h4>
+            </div>
+        </div>
+        <div class="newsWrap">
+            <div class="titleBlock">
+                <h4>The Kennetic Way</h4>
+            </div>
+            <div class="newsWrap">
+                <?php include 'inc/zone-content.php'; ?>
+            </div>
+        </div>
+        <div class="articleMenu">
+            <div class="titleBlock">
+                <h4>Kennetic Journal</h4>
+            </div>
+        </div>
+    </div>
+</body>
+<?php include 'inc/footer.php'; ?>
+
+</html>
